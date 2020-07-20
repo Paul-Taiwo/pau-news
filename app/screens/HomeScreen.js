cf1c5020653e1d308a38d9eaccca0e7d990f3728
Paul Taiwo <ayopaulot@gmail.com>
2020-07-20 06:36:27 +0100

    add dummy data

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -7,6 +7,22 @@ import SourcesCard from "../components/SourceCard";
 import NewsCard from "../components/NewsCard";
 import { getBgColor } from "../utils";
 
+const dummyData = {
+  author: "Thomas Simms",
+  content:
+    "Coming every Sunday, Hodlers Digest will help you track every single important news story that happened this week. The best (and worst) quotes, adoption and regulation highlights, leading coins, pred… [+9925 chars]",
+  description:
+    "Top celebrities, politicians and businesses are caught up in a “social engineering attack” on Twitter — with theories swirling around that it could have been an “inside job”",
+  publishedAt: "2020-07-19T20:00:00Z",
+  source: { id: null, name: "Cointelegraph" },
+  title:
+    "Twitter Hack Special: $120K Stolen, FBI Investigate, Calls to Ban BTC — Hodler’s Digest, July 13–19 - Cointelegraph",
+  url:
+    "https://cointelegraph.com/news/twitter-hack-special-120k-stolen-fbi-investigate-calls-to-ban-btc-hodlers-digest-july-1319",
+  urlToImage:
+    "https://s3.cointelegraph.com/storage/uploads/view/2e5dcabca32d6ad40f68891f7431602c.jpg",
+};
+
 const HomeScreen = ({ name, navigation }) => {
   const Width = Dimensions.get("window").width;
   const handleNavigation = () => {
@@ -63,11 +79,11 @@ const HomeScreen = ({ name, navigation }) => {
               backgroundColor: "#f2f2f2",
             },
           ]}>
-          <NewsCard handlePress={handleNavigation} />
-          <NewsCard handlePress={handleNavigation} />
-          <NewsCard handlePress={handleNavigation} />
-          <NewsCard handlePress={handleNavigation} />
-          <NewsCard handlePress={handleNavigation} />
+          <NewsCard data={dummyData} handlePress={handleNavigation} />
+          <NewsCard data={dummyData} handlePress={handleNavigation} />
+          <NewsCard data={dummyData} handlePress={handleNavigation} />
+          <NewsCard data={dummyData} handlePress={handleNavigation} />
+          <NewsCard data={dummyData} handlePress={handleNavigation} />
         </View>
       </ScrollView>
     </SafeAreaView>
