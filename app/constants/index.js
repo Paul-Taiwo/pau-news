e35996efcbdf1915e18bc26eb77e8bfb4fd14726
Paul Taiwo <ayopaulot@gmail.com>
2020-07-21 01:30:51 +0100

    update constants

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,11 +1,15 @@
+import { ENV_API_URL, ENV_API_KEY1, ENV_API_KEY2 } from "@env";
+
 const bgColors = ["#2301a6", "#cc822c", "#375057", "#030128"];
 
 const NAME = "news";
 const PAGESIZE = 20;
 
+// console.log("============>", ENV_API_KEY1);
+
 //API URL
-const API_URL = "https://newsapi.org/v2";
-const API_KEY = "?apiKey=";
+const API_URL = ENV_API_URL;
+const API_KEY = `?apiKey=${ENV_API_KEY1}`;
 const API_PARAMS = `&pageSize=${PAGESIZE}`;
 
 //API End Points
